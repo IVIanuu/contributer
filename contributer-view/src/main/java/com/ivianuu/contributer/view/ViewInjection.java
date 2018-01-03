e853f5301f329475442c1068f2c56bbf892a7de5
IVIanuu <IVIanuu@gmail.com>
2018-01-03 22:43:29 +0100

    Check view parents in view injection

--- contributer-view/src/main/java/com/ivianuu/contributer/view/ViewInjection.java
+++ contributer-view/src/main/java/com/ivianuu/contributer/view/ViewInjection.java
@@ -1,6 +1,9 @@
 package com.ivianuu.contributer.view;
 
+import android.app.Activity;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewParent;
 
 import dagger.android.AndroidInjector;
 
@@ -30,14 +33,24 @@ public final class ViewInjection {
     }
 
     private static HasViewInjector findHasViewInjector(View view) {
-        if (view.getContext() instanceof HasViewInjector) {
+        if (view.getParent() != null) {
+            ViewParent parent = view.getParent();
+            if (parent instanceof HasViewInjector) {
+                return (HasViewInjector) parent;
+            }
+
+            while ((parent = parent.getParent()) != null) {
+                if (parent instanceof HasViewInjector) {
+                    return (HasViewInjector) parent;
+                }
+            }
+        } else if (view.getContext() instanceof HasViewInjector) {
             return (HasViewInjector) view.getContext();
         } else if (view.getContext().getApplicationContext() instanceof HasViewInjector) {
             return (HasViewInjector) view.getContext().getApplicationContext();
-        } else {
-            throw new IllegalArgumentException(String.format(
-                    "No injector was found for %s", view.getClass().getCanonicalName()));
         }
-    }
 
+        throw new IllegalArgumentException(String.format(
+                "No injector was found for %s", view.getClass().getCanonicalName()));
+    }
 }
